116ae560faa41135703f8331cece0f9e336190bd
DeepiGit <85212364+DeepiGit@users.noreply.github.com>
2021-06-02 19:04:31 +0530

    question

---
+++ frequencySorting.c
@@ -0,0 +1,10 @@
+/*
+Sort descending order based on frequency and for same frequency sort ascending
+
+Sample Input 
+n = 10(size of the array)
+arr = 2 2 3 2 1 3 1 4 4 4 
+
+Sample output
+2 2 2 4 4 4 1 1 2 2 
+*/
