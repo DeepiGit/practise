d60bd45be4da50dbba821a8013cfe38e66999d5f
DeepiGit <85212364+DeepiGit@users.noreply.github.com>
2021-06-02 19:10:16 +0530

    main function is created

--- frequencySorting.c
+++ frequencySorting.c
@@ -10,3 +10,14 @@ Sample output
 */
 
 #include<stdio.h>
+int main()
+{
+    int num, i, j;
+    scanf("%d",&num);
+    int arr[num];
+    for(i = 0; i < num; i++)
+        scanf("%d",&arr[i]);
+   return 0;
+
+}
+
